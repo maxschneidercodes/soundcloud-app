9d6a7640db86b57fb122c6af2c114aa05c457b3b
Max Schneider <maxschneidercodes@gmail.com>
2023-04-26 12:59:50 +0200

    init

---
+++ src/includes/firebase.js
@@ -0,0 +1,36 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  appId: import.meta.env.VITE_appId,
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+db.enablePersistence().catch((error) => {
+  console.log(`Firebase persistence error ${error.code}`);
+});
+
+const usersCollection = db.collection("users");
+const songsCollection = db.collection("songs");
+const commentsCollection = db.collection("comments");
+
+export {
+  auth,
+  db,
+  usersCollection,
+  songsCollection,
+  commentsCollection,
+  storage,
+};
